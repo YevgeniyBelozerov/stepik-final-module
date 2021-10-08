c0c497aa796836a3c4ed6f34a61b2c76decb18bd
EvgBelozeroff <yevgeniy.belozerov@gmail.com>
2021-10-09 01:08:03 +0700

    Добавил параметризацию в тест с корзиной, пометил тест с багом

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,11 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo_offer', ['0', '1', '2', '3', '4', '5', '6',
+                                         pytest.param(7, marks=pytest.mark.xfail(reason='в 7 тесте баг')), '8', '9'])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open(browser, link)
     page.should_be_correct_addition_cart()
