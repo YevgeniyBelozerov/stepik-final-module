637aada9a375408f5cac4babcb6223d22c642317
EvgBelozeroff <yevgeniy.belozerov@gmail.com>
2021-10-11 21:06:57 +0700

    пометил падающие тесты, указал причину

--- test_product_page.py
+++ test_product_page.py
@@ -2,36 +2,38 @@ from .pages.product_page import ProductPage
 import pytest
 
 
-# @pytest.mark.parametrize('promo_offer', ['0', '1', '2', '3', '4', '5', '6',
-#                                          pytest.param(7, marks=pytest.mark.xfail(reason='в 7 тесте баг')), '8', '9'])
-# def test_guest_can_add_product_to_basket(browser, promo_offer):
-#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-#     page = ProductPage(browser, link)
-#     page.open(browser, link)
-#     page.should_be_correct_addition_cart()
+@pytest.mark.parametrize('promo_offer', ['0', '1', '2', '3', '4', '5', '6',
+                                         pytest.param(7, marks=pytest.mark.xfail(reason='в 7 тесте баг')), '8', '9'])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    page = ProductPage(browser, link)
+    page.open(browser, link)
+    page.should_be_correct_addition_cart()
 
 
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser, link)
-#     page.open(browser, link)
-#     page.should_be_add_item_into_cart()
-#     page.should_not_be_success_message()
-#
-#
-# def test_guest_cant_see_success_message(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser, link)
-#     page.open(browser, link)
-#     page.should_not_be_success_message()
-#
-#
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser, link)
-#     page.open(browser, link)
-#     page.should_be_add_item_into_cart()
-#     page.should_dissapear_of_success_message()
+@pytest.mark.xfail(reason='Учебный тест, который всегда должен падать')
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open(browser, link)
+    page.should_be_add_item_into_cart()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open(browser, link)
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason='Учебный тест, который всегда должен падать')
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open(browser, link)
+    page.should_be_add_item_into_cart()
+    page.should_dissapear_of_success_message()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
